a426cea6d2bd5f2c9073175216088152ed37119d
qindongyan <592325672@qq.com>
2023-09-19 17:19:21 +0800

    Create call-apply-bind.js

---
+++ call-apply-bind.js
@@ -0,0 +1,34 @@
+Function.prototype.myCall = function(context,...args){
+  if (typeof this !== 'function'){
+    return new TypeError('error');
+    context = context || window;
+    const key = Symbol()
+    context[key] = this;
+    context[key](...args);
+    delete context[key];
+    return result;
+  }
+}
+Function.prototype.myApply = function(context,args){
+  if (typeof this !== 'functon'){
+    return new TypeError('error');
+  }
+  context = context || window;
+  const key = Symbol();
+  context[key] = this;
+  const result = context[key](...args);
+  delete context[key]
+  return result;
+}
+
+Function.prototype.myBind = function(context,...args){
+  if (typeof this !== 'functon'){
+    return new TypeError('error');
+  }
+  context = context || window;
+  const key = Symbol();
+  context[key] = this;
+  return new function(){
+    context[key](...args)
+  }
+}
